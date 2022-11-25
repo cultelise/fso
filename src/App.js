2a35aca3a0701e01500a26fab80a4e59164e33c4
elise <115508722+cultelise@users.noreply.github.com>
2022-11-25 16:47:18 -0600

    fso part 2c persons

--- src/App.js
+++ src/App.js
@@ -3,16 +3,34 @@ import { useState } from "react";
 import Input from "./components/Input";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import axios from "axios";
+import { useEffect } from "react";
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", id: 1, number: "123-321-4567" }, { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+  
 
   const getNewName = (event) => {
     setNewName(event.target.value);
@@ -69,6 +87,7 @@ const App = () => {
       />
       <h2>Numbers</h2>
       <Persons persons={getFilteredEntries()} />
+      <p>{props.notes}</p>
     </div>
   );
 };
