10e29202f7a9747cb7be3d18da3a19245e964b60
elise <115508722+cultelise@users.noreply.github.com>
2022-11-25 18:47:36 -0600

    Filter progress

--- src/App.js
+++ src/App.js
@@ -5,48 +5,40 @@ import { useEffect } from "react";
 import Filter from "./components/Filter";
 import Countries from "./components/Countries";
 
-const App = (props) => {
-  const [countries, setCountries] = useState([])
-  const [newFilter, setNewFilter] = useState("")
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [newFilter, setNewFilter] = useState("");
 
   useEffect(() => {
-    axios
-    .get('https://restcountries.com/v3.1/all')
-    .then(response => {
-      setCountries(response.data)
-    })
-  }, [])
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
 
   const getNewFilter = (event) => {
     setNewFilter(event.target.value);
     getFilteredEntries(event.target.value);
   };
 
-  const getFilteredList = (value) => {
-    console.log(value)
-    setNewFilter(value);
-    getFilteredEntries();
-  };
-
-  const filteredList = (value) => {
-    if (countries === []) return;
-    if (countries !== [] && value === undefined) return countries;
-    else return getFilteredList(value);
-  }
-
   const getFilteredEntries = () => {
-     return countries.filter((x) => {
-      return x.name.common.toLowerCase().includes(newFilter.toLowerCase())
-      }
-    );
-  };
+    if (newFilter === "") {
+      return []
+    } else {
+      if (
+        countries.filter((x)=>x.name.common.toLowerCase().includes(newFilter.toLowerCase())).length >
+        10
+      ) {
+        console.log(countries)
+        return [{name: {common: "Be More Specific!"}}];
+      } else return countries.filter((x)=>x.name.common.toLowerCase().includes(newFilter.toLowerCase()))
+  };}
 
   return (
     <div>
-      <Filter label={'countries'} value={newFilter} onChange={getNewFilter}/>
+      <Filter label={"countries"} value={newFilter} onChange={getNewFilter} />
       <Countries countries={getFilteredEntries()} />
     </div>
-  )
+  );
 };
 
 export default App;
